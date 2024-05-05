f93c296cdb4e6b822dc99d098797b36be1abd41d
NXKXTA <112431325+NXKXTA@users.noreply.github.com>
2024-05-06 00:35:15 +0300

    num_2.py

--- num_2.py
+++ num_2.py
@@ -1,48 +1,47 @@
 import csv
 import os
+from docx import Document
 from docxtpl import DocxTemplate
+
+marathon_list = list()
 
 marathon_path = './data_marathon.csv'
 if not os.path.exists(marathon_path):
     print("Нет файла с марафоном")
     exit()
 
-with open(marathon_path, 'r', encoding="utf-8") as input_csv_file:
-    reader = csv.reader(input_csv_file, delimiter=",")
-    all_marathons = list(reader)
+with open(marathon_path, 'r', encoding="utf-8") as csvfile:
+    reader = csv.reader(csvfile, delimiter=',')
+    for row in reader:
+        marathon_data = list()
+        marathon_data.append(int(row[0]))
+        marathon_data = marathon_data + row[1:]
+        marathon_list.append(marathon_data)
 
-current_year = None
+marathon_list.sort()
+
 template = DocxTemplate("Shablon.docx")
 
-number = 0  # Для отслеживания номера марафона
-
-for marathon in all_marathons:
-    number += 1  # Увеличиваем номер марафона
-    year = marathon[0]
-    city = marathon[5]
-    winner = marathon[2]
-    winner_name = marathon[1]
-    time = marathon[4]
-
-    # Проверяем, начинается ли новый год
-    if year != current_year:
-        # Если это не первый год, добавляем разрыв страницы
-        if current_year is not None:
-            template.add_page_break()
-
-        current_year = year
-
-    # Добавляем информацию о марафоне
-
+document = Document()
+last = marathon_list[0][0]
+number = 0
+for i in marathon_list:
+    number += 1
     context = {
         'number': number,
-        'year': year,
-        'city': city,
-        'men': winner_name,
-        'time': time,
+        "year": i[0],
+        "city": i[5],
+        "name": ("-мужчины " if i[2] == "Male" else "-женщины ") + str(i[1]),
+        "time": i[4]
     }
+    if last != i[0]:
+        document.add_page_break()
 
     template.render(context)
+    temp_doc = template.docx
+    for paragraph in temp_doc.paragraphs:
+        document.add_paragraph(paragraph.text)
 
-# Сохраняем результат в одном файле
-template.save("Marathon_results.docx")
+    last = i[0]
+
+document.save("Marathon_results.docx")
